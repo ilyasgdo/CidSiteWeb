21eb421b082bf1af69603af13f0dad1f744b7029
William <willoyou20@gmail.com>
2023-12-11 07:32:24 +0100

    Implémentation des deux pages galerie et photo.

---
+++ CidSiteWeb/assets/js/script-photo.js
@@ -0,0 +1,8 @@
+function showPictureForm(){
+    var formulaire = document.getElementById("formulaire-photo");
+    if (formulaire.style.display === "none") {
+      formulaire.style.display = "block";
+    } else {
+      formulaire.style.display = "none";
+    }
+}
